74834105c5c4303d9d58e6ef0c1f85c21167a4d7
Monastyrev Ivan <ikloster@yandex.ru>
2018-02-22 09:43:34 +0300

    Update header

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,43 @@
 import '../css/app.scss'
+import $ from 'jquery'
+
+$(document).ready( function ( ) {
+    const $window = $( window );
+    const header = '.header';
+    const active = 'header_active';
+    const headerMenuList = '#header-menu-list';
+    const menuMobile = 'menu_mobile';
+
+    $( '#header-menu-btn' ).click( function ( e ) {
+        console.log('header-menu');
+        e.preventDefault();
+        $( '.menu-icon-toggle' ).toggleClass( 'menu-icon-toggle_expanded' );
+        $( headerMenuList ).toggleClass( 'menu_active' );
+    } );
+
+
+
+    function setHeader() {
+        console.log('header', $( this ).width( ));
+        if( $( this ).width( ) < 1024) {
+            $( headerMenuList ).addClass( menuMobile );
+        } else {
+            $( headerMenuList ).removeClass( menuMobile );
+            $window.scroll( function ( ) {
+                if ( $window.scrollTop( ) > 10 ) {
+                    $( header ).addClass( active );
+                } else {
+                    $( header ).removeClass( active );
+                }
+            })
+        }
+    }
+
+    setHeader();
+
+    $window.resize(function () {
+        setHeader();
+    })
+
+
+} );
