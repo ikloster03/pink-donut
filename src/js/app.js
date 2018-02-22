11199e05bc8c70d3cd7b496bd4b8d06e3e2cdec6
Monastyrev Ivan <ikloster@yandex.ru>
2018-02-22 15:15:31 +0300

    Fix and update header

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import '../css/app.scss'
 import $ from 'jquery'
+import TrackedMenu from './tracked'
 
 $(document).ready( function ( ) {
     const $window = $( window );
@@ -37,7 +38,30 @@ $(document).ready( function ( ) {
 
     $window.resize(function () {
         setHeader();
-    })
+    });
 
+    $( headerMenuList + ' li' ).on( 'click', 'a', function( e ) {
+        e.preventDefault( );
+
+        let id = $( this ).attr( 'href' );
+        let top = $( id ).offset( ).top - $( headerMenuList + ' li' ).outerHeight( true );
+
+
+        $( 'body, html' ).animate({
+            scrollTop: top
+        }, 1500);
+
+    });
+
+    const headerColors = {
+        'section-home': 'header_yellow',
+        'section-what-i-do': 'header_white',
+        'section-my-works': 'header_white',
+        'section-about-me': 'header_white',
+        'section-contact': 'header_blue',
+    };
+
+
+    const trackedMenu = new TrackedMenu( header, headerColors, headerMenuList, 'menu__item_active', '.section_tracked' );
 
 } );
